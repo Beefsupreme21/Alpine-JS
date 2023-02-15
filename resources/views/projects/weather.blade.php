2dffe86ee86fdd3f9c58b6f586423c6c5c0c756e
Beefsupreme21 <beefsupreme21@hotmail.com>
2023-02-14 21:55:17 -0700

    wip

--- resources/views/projects/weather.blade.php
+++ resources/views/projects/weather.blade.php
@@ -1,51 +1,104 @@
 <x-layout>
-    <div x-data="weather">
-
-        <button x-on:click="fetchWeather()" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">Fetch Weekly Forecase</button>
-        <div>
-            Current Weather
-            <div x-text="weather.current_weather.temperature"></div>
-        </div>
-        <template x-for="(day, index) in weather.daily.time" :key="index">
-            <div class="mt-4 p-4 border border-gray-300">
-                <div class="text-lg font-bold">
-                    <span x-text="getDayOfWeek(day)"></span>
-                    <span x-text="new Date(day * 1000).toLocaleDateString()"></span>
-                    <img x-bind:src="getWeatherIcon(weather.daily.weathercode[index])" alt="weather icon">
-                </div>
-                <div class="mt-2">
-                    Weather Code: <span x-text="weather.daily.weathercode[index]"></span>
-                </div>
-                <div class="mt-2">
-                    Time: <span x-text="day"></span>
-                </div>
-                <div class="mt-2">
-                    Temperature Max: <span x-text="weather.daily.temperature_2m_max[index]"></span>
-                </div>
-                <div class="mt-2">
-                    Temperature Min: <span x-text="weather.daily.temperature_2m_min[index]"></span>
-                </div>
-                <div class="mt-2">
-                    Apparent Temperature Max: <span x-text="weather.daily.apparent_temperature_max[index]"></span>
-                </div>
-                <div class="mt-2">
-                    Apparent Temperature Min: <span x-text="weather.daily.apparent_temperature_min[index]"></span>
-                </div>
-                <div class="mt-2">
-                    Precipitation Sum: <span x-text="weather.daily.precipitation_sum[index]"></span>
+    <div x-data="weather" x-init="fetchWeather()">
+        <div class="flex mx-auto w-max py-6">
+            <div class="flex border-r border-gray-600 pr-4 items-center">
+                <img x-bind:src="getWeatherIcon(weather.current_weather.weathercode)" alt="weather icon" class="w-32">
+                <div class="text-4xl pl-4" x-text="Math.round(weather.current_weather.temperature)"></div>
+                <span class="text-2xl">&deg;</span>
+            </div>
+            <div class="px-4">
+                <div class="temperature">
+                    <p class="label">HIGH</p>
+                    <span class="text-2xl" x-text="Math.round(weather.daily.temperature_2m_max[0])"></span>
+                    <span class="text-2xl -ml-1">&deg;</span>
+                    <p class="label">LOW</p>
+                    <span class="text-2xl" x-text="Math.round(weather.daily.temperature_2m_min[0])"></span>
+                    <span class="text-2xl -ml-1">&deg;</span>
                 </div>
             </div>
-        </template>
-    </div>
-    
+            <div class="px-4">
+                <div class="temperature">
+                    <p class="label">FL HIGH</p>
+                    <span class="text-2xl" x-text="Math.round(weather.daily.apparent_temperature_max[0])"></span>
+                    <span class="text-2xl -ml-1">&deg;</span>
+                    <p class="label">FL LOW</p>
+                    <span class="text-2xl" x-text="Math.round(weather.daily.apparent_temperature_min[0])"></span>
+                    <span class="text-2xl -ml-1">&deg;</span>
+                </div>
+            </div>      
+            <div class="px-4">
+                <div>
+                    <p class="label">Wind</p>
+                    <span class="text-2xl" x-text="weather.current_weather.windspeed"></span>
+                    <span class="-ml-1">mph</span>
+                </div>
+                <div>
+                    <p class="label">Precip</p>
+                    <span class="text-2xl" x-text="weather.daily.precipitation_sum[0]"></span>
+                    <span class="-ml-1">in</span>
+                </div>
+            </div>
+        </div>
+        <div class="grid grid-cols-7 mb-5">
+            <template x-for="(day, index) in weather.daily.time" :key="index">
+                <div x-on:click="selectedDay = day" class="p-4 border border-gray-600 rounded-lg shadow-lg flex flex-col justify-between cursor-pointer hover:bg-gray-800">
+                    <div class="flex items-center h-full">
+                        <img :src="getWeatherIcon(weather.daily.weathercode[index])" alt="weather icon" class="w-32">
+                    </div>
+                    <div class="text-center">
+                        <div x-text="getDayOfWeek(day)"></div>
+                        <span class="text-2xl" x-text="Math.round(weather.daily.temperature_2m_max[index])"></span>
+                        <span class="text-2xl">&deg;</span>
 
+                    </div>
+                </div>
+            </template>
+        </div>
+        <div x-show="selectedDay">
+            <template x-for="(hour, hourIndex) in weather.hourly.time" :key="hourIndex">
+                <template x-if="isWithinDay(hour, selectedDay)">
+                    <div x-bind:class="hourIndex % 2 === 0 ? 'bg-gray-700' : 'bg-gray-800'" class="grid grid-cols-6 justify-items-center justify-between p-1 items-center text-center">
+                        <div>
+                            <div x-text="getDayOfWeek(selectedDay).toUpperCase()" class="text-sm font-bold"></div>
+                            <div x-text="new Date(hour * 1000).toLocaleTimeString([], { hour: 'numeric' })" class="text-lg"></div>
+                        </div>
+                        <img x-bind:src="getWeatherIcon(weather.hourly.weathercode[hourIndex])" alt="weather icon" class="w-8">
+                        <div>
+                            <div class="text-sm font-bold">TEMP</div>
+                            <div>
+                                <span x-text="Math.round(weather.hourly.temperature_2m[hourIndex])"></span>
+                                <span class="-ml-1">&deg;</span>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="text-sm font-bold">FL TEMP</div>
+                            <span x-text="Math.round(weather.hourly.temperature_2m[hourIndex])"></span>
+                            <span class="-ml-1">&deg;</span>
+                        </div>
+                        <div>
+                            <div class="text-sm font-bold">WIND</div>
+                            <span x-text="weather.hourly.windspeed_10m[hourIndex]"></span>
+                            <span class="-ml-1">mph</span>
+                        </div>
+                        <div>
+                            <div class="text-sm font-bold">PRECIP</div>
+                            <span x-text="weather.hourly.precipitation[hourIndex]"></span>
+                            <span class="-ml-1">in</span>
+                        </div>
+                    </div>
+                </template>
+            </template>
+        </div>
+    </div>
+      
     <script>
         document.addEventListener('alpine:init', () => {
             Alpine.data('weather', () => ({
                 weather: null,
+                selectedDay: null,
 
                 getDayOfWeek(timestamp) {
-                    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+                    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
                     const date = new Date(timestamp * 1000);
                     return daysOfWeek[date.getUTCDay()];
                 },
@@ -53,19 +106,36 @@
                 fetchWeather() {
                     axios.get("https://api.open-meteo.com/v1/forecast?latitude=39.74&longitude=-104.98&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FDenver")
                         .then(response => {
-                        this.weather = response.data;
+                            this.weather = response.data;
                         });
                 },
 
+                isWithinDay(timestamp, day) {
+                    const selectedDate = new Date(day * 1000);
+                    const date = new Date(timestamp * 1000);
+                    return (
+                        date.getFullYear() === selectedDate.getFullYear() &&
+                        date.getMonth() === selectedDate.getMonth() &&
+                        date.getDate() === selectedDate.getDate()
+                    );
+                },
+
                 getWeatherIcon(code) {
-                    switch (code) {
-                    case 0:
+                    if (code === 0 || code === 1) {
                         return '/images/weather-icons/sun.svg';
-                    case 1:
-                    case 2:
-                    case 3:
-                        return '/images/weather-icons/sun.svg';
-                    default:
+                    } else if (code === 2) {
+                        return '/images/weather-icons/cloud-sun.svg';
+                    } else if (code === 3) {
+                        return '/images/weather-icons/cloud.svg';
+                    } else if (code === 45 || code === 48) {
+                        return '/images/weather-icons/smog.svg';
+                    } else if (code >= 51 && code <= 67 || code === 80 || code === 81 || code === 82) {
+                        return '/images/weather-icons/cloud-showers-heavy.svg';
+                    } else if (code === 71 || code === 73 || code === 75 || code === 77 || code === 85 || code === 86) {
+                        return '/images/weather-icons/snowflake.svg';
+                    } else if (code === 95 || code === 96 || code === 99) {
+                        return '/images/weather-icons/cloud-bolt.svg';
+                    } else {
                         return 'path/to/default-icon.svg';
                     }
                 },
