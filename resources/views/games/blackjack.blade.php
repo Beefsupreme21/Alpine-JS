cae19a1bc9c5661881ac0c82bff5f295fbfef0e3
Beefsupreme21 <beefsupreme21@hotmail.com>
2023-03-15 23:25:50 -0600

    wip

--- resources/views/games/blackjack.blade.php
+++ resources/views/games/blackjack.blade.php
@@ -7,49 +7,95 @@
                 <template x-for="(card, index) in dealerCards">
                     <img x-bind:src="showCardValues[index] ? getCardImage(card) : '/images/cards/back-blue.png'" class="inline-block bg-white border border-gray-400 rounded-md p-1 m-1" alt="">
                 </template>
-                <template x-if="!gameStarted" class="mt-4">
-                    <button x-on:click="dealCards()" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">Deal Cards</button>
-                </template>
             </div>
         </div>
+        <div>
+            <button x-on:click="if (currentBet < playerBalance) { currentBet += 1 }">
+                <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="50" cy="50" r="45" fill="#2196F3" stroke="black" stroke-width="2" />
+                    <circle cx="50" cy="50" r="30" fill="white" stroke="black" stroke-width="2" />
+                    <text x="50" y="58" font-size="24" font-family="Arial" font-weight="bold" text-anchor="middle">1</text>
+                </svg>
+            </button>
+            <button x-on:click="if (currentBet < playerBalance) { currentBet += 5 }">
+                <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="50" cy="50" r="45" fill="#FF0000" stroke="black" stroke-width="2" />
+                    <circle cx="50" cy="50" r="30" fill="white" stroke="black" stroke-width="2" />
+                    <text x="50" y="58" font-size="24" font-family="Arial" font-weight="bold" text-anchor="middle">5</text>
+                </svg>
+            </button>
+            <button x-on:click="if (currentBet < playerBalance) { currentBet += 25 }">
+                <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="50" cy="50" r="45" fill="#008000" stroke="black" stroke-width="2" />
+                    <circle cx="50" cy="50" r="30" fill="white" stroke="black" stroke-width="2" />
+                    <text x="50" y="58" font-size="24" font-family="Arial" font-weight="bold" text-anchor="middle">25</text>
+                </svg>
+            </button>
+            <button x-on:click="if (currentBet < playerBalance) { currentBet += 100 }">
+                <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="50" cy="50" r="45" fill="#000000" stroke="black" stroke-width="2" />
+                    <circle cx="50" cy="50" r="30" fill="white" stroke="black" stroke-width="2" />
+                    <text x="50" y="58" font-size="24" font-family="Arial" font-weight="bold" text-anchor="middle">100</text>
+                </svg>
+            </button>
+        </div>
 
+
+
+
+          
+        <div class="flex flex-grow justify-between items-center">
+            <div>
+                <h3 class="text-lg font-bold mb-2">Balance:</h3>
+                <span x-text="playerBalance" class="text-xl font-bold"></span>
+            </div>
+    
+            <div>
+                <span class="font-semibold text-3xl mx-4 w-20 text-center" x-text="currentBet"></span>
+            </div>
+    
+            <div>
+                <button x-on:click="currentBet = 0" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Clear</button>
+                <button x-bind:disabled="currentBet === 0" x-on:click="placeBet()" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Play</button>
+            </div>
+        </div>
+    
         <div class="flex-grow">
             <div class="text-center">
                 <h2 class="mb-4 font-bold text-lg">Player 1</h2>
-                <template x-if="playerOneCards.some(card => card.value === 'A')">
-                    <span class="text-lg font-bold mb-2"><span x-text="playerOneHandValue - 10"></span> or </span>
+                <template x-if="playerCards.some(card => card.value === 'A')">
+                    <span class="text-lg font-bold mb-2"><span x-text="playerHandValue - 10"></span> or </span>
                 </template>
-                <span x-text="playerOneHandValue" class="text-lg font-bold mb-2"></span>
+                <span x-text="playerHandValue" class="text-lg font-bold mb-2"></span>
                 <div>
-                    <template x-for="card in playerOneCards">
+                    <template x-for="card in playerCards">
                         <img x-bind:src="getCardImage(card)" class="inline-block bg-white border border-gray-400 rounded-md p-1 m-1" alt="">
                     </template>
-                    <div x-show="gameStarted">
+                    <div>
                         <button x-on:click="playerHit()" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-4">Hit</button>
                         <button x-on:click="playerStand()" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4 ml-4">Stand</button>
                     </div>
                 </div>
-                <p x-text="resultsMessage" class="text-lg font-bold mt-4"></p>
-                <div x-show="playerOneCards && resultsMessage" class="mt-4">
-                    <button x-on:click="resetGame()" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Deal Again</button>
-                </div>
             </div>
         </div>
     </div>
-    
+
     <script>
         document.addEventListener('alpine:init', () => {
             Alpine.data('blackjack', () => ({
                 deck: [],
-                playerOneCards: [],
+                playerCards: [],
                 dealerCards: [],
-                playerOneHandValue: 0,
+                playerHandValue: 0,
                 dealerHandValue: 0,
+                playerBalance: 1000,
+                currentBet: 0,
                 showCardValues: [false, true, true, true, true], 
                 resultsMessage: null,
-                gameStarted: false,
+                gamePhase: 'betting',
 
                 startGame() {
+                    this.gamePhase = 'betting';
                     this.deck = this.getDeck();
                 },
         
@@ -108,17 +154,16 @@
                 },
         
                 dealCards() {
-                    this.gameStarted = true;
-                    this.drawCard('playerOne');
+                    this.drawCard('player');
                     this.drawCard('dealer');
-                    this.drawCard('playerOne');
+                    this.drawCard('player');
                     this.drawCard('dealer');
                 },
         
                 drawCard(player) {
                     const card = this.deck.shift();
-                    if (player === 'playerOne') {
-                        this.playerOneCards.push(card);
+                    if (player === 'player') {
+                        this.playerCards.push(card);
                         this.calculateHandValue(player);
                     } else if (player === 'dealer') {
                         this.dealerCards.push(card);
@@ -130,19 +175,15 @@
                     }
                 },
 
-                hit() {
+                playerHit() {
                     const card = this.deck.shift();
-                    this.playerOneCards.push(card);
-                    this.calculateHandValue('playerOne');
-                    if (this.playerOneHandValue > 21) {
+                    this.playerCards.push(card);
+                    this.calculateHandValue('player');
+                    if (this.playerHandValue > 21) {
                         this.dealerWins();
-                    } else if (this.playerOneHandValue == 21) {
+                    } else if (this.playerHandValue == 21) {
                         this.dealerTurn();
                     }
-                },
-
-                playerHit() {
-                    this.hit();
                 },
 
                 playerStand() {
@@ -158,9 +199,9 @@
                     }
                     if (this.dealerHandValue > 21) {
                         this.playerWins();
-                    } else if (this.dealerHandValue == this.playerOneHandValue) {
+                    } else if (this.dealerHandValue == this.playerHandValue) {
                         this.playerTies();
-                    }else if (this.dealerHandValue >= this.playerOneHandValue) {
+                    }else if (this.dealerHandValue >= this.playerHandValue) {
                         this.dealerWins();
                     } else {
                         this.playerWins();
@@ -168,17 +209,31 @@
                 },
 
                 playerWins() {
-                    this.resultsMessage = "Player wins!"
+                    this.resultsMessage = "Player wins!";
+                    this.playerBalance += this.currentBet * 2;
+                    this.currentBet = 0;
+                    this.gamePhase = 'gameOver';
                 },
 
                 playerTies() {
                     this.revealCardValues();
-                    this.resultsMessage = "Push"
+                    this.resultsMessage = "Push";
+                    this.playerBalance += this.currentBet;
+                    this.currentBet = 0;
+                    this.gamePhase = 'gameOver';
                 },
 
                 dealerWins() {
                     this.revealCardValues();
-                    this.resultsMessage = "Dealer wins!"
+                    this.resultsMessage = "Dealer wins!";
+                    this.currentBet = 0;
+                    this.gamePhase = 'gameOver';
+                },
+
+                placeBet() {
+                    this.playerBalance -= this.currentBet;
+                    this.gamePhase = 'playing'; // Change phase to 'playing' after placing a bet
+                    this.dealCards();
                 },
 
                 revealCardValues() {
@@ -186,14 +241,15 @@
                 },
 
                 resetGame() {
-                    this.playerOneCards = [];
+                    this.playerCards = [];
                     this.dealerCards = [];
-                    this.playerOneHandValue = 0;
+                    this.playerHandValue = 0;
                     this.dealerHandValue = 0;
                     this.resultsMessage = null;
                     this.deck = this.getDeck();
-                    this.dealCards();
+                    // this.dealCards();
                     this.showCardValues[0] = false;
+                    this.gamePhase = 'betting';
                 },
 
                 sumCardValues(cards) {
@@ -213,8 +269,8 @@
                 },
 
                 calculateHandValue(player) {
-                    if (player === 'playerOne') {
-                        this.playerOneHandValue = this.sumCardValues(this.playerOneCards);
+                    if (player === 'player') {
+                        this.playerHandValue = this.sumCardValues(this.playerCards);
                     } else if (player === 'dealer') {
                         this.dealerHandValue = this.sumCardValues(this.dealerCards);
                     }
